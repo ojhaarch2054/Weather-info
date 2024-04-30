53976a2c10e67966f1447bd5fbf3c941eefa1781
Archana Ojha <archana.ozza@gmail.com>
2024-04-30 11:44:11 +0300

    use math.round method to round up the temprature

--- my-weather-app/src/App.js
+++ my-weather-app/src/App.js
@@ -6,7 +6,6 @@ import './App.css';
 //import search icon
 import SearchIcon from './search.svg';
 
-//aaf767abe47c357a819ebb75ad9636d7
 //API_URL based on openweather
 const API_URL = 'https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=aaf767abe47c357a819ebb75ad9636d7'
 
@@ -18,33 +17,40 @@ function App() {
   const [searchTerm, setSearchTerm] = useState('');
 //weather_info is an async function that fetches weather data for a given city
   const weather_info = async (cityName) => {
-//The response from the API is stored in the response variable
+//response from the API is stored in the response variable
     const response = await fetch(`${API_URL}&q=${cityName}`);
-//The JSON data from the response is stored in the data variable
+//JSON data from the response is stored in the data variable
     const data = await response.json();
     console.log(data);
-//The weatherData state is updated with the data from the API
+//weatherData state is updated with the data from the API
     setWeatherData(data);
   };
-  // call to the useEffect hook
+  //call to the useEffect hook
   useEffect(() => {
-    weather_info('Finland');
+    weather_info('');
     }, []);
  //return contains the JSX that will be rendered by the App component
   return (
     <div className="app">
-      <h1>Current Weather</h1>{/* Title of the page */}
+      <h1>Current Weather</h1>{/*Title of the page */}
       <div className="search">
-              {/* Text input for the user to enter a location */}
+          {/* Text input for the user to enter a location */}
         <input type="text" placeholder='Search Location' value = {searchTerm}
             onChange = {(e) => setSearchTerm(e.target.value)}/>
-                  {/* Search button that triggers the weather_info function when clicked */}
+          {/* Search button that triggers the weather_info function when clicked */}
             <img src={SearchIcon} alt="Search"
             onClick={() => weather_info(searchTerm)} />
       </div>
       {/*To fetch the jsx into return */}
-      {weatherData && <WeatherCard weatherData={weatherData} />}
-    </div>
-  );
-  }
+      {weatherData.main && (
+      <>
+        <WeatherCard weatherData={weatherData} />
+      
+      </>
+    )}
+  </div>
+);
+}
+
+
 export default App;
--- my-weather-app/src/Weather.jsx
+++ my-weather-app/src/Weather.jsx
@@ -3,22 +3,22 @@ import React from 'react';
 const WeatherCard = ({ weatherData }) => {
     return (
       <div className="weather">
-        {/*  In this part it will display city name and weather description */}
+        {/*in this part it will display city name and weather description */}
         <div>
           <h3>{weatherData.name}</h3>
           <p>{weatherData.weather[0].main}</p>
           <p>{weatherData.weather[0].description}</p>
         </div>
-        {/* this section is for displaying icon*/}
+        {/*this section is for displaying icon*/}
         <div>
           <img
             src={`http://openweathermap.org/img/wn/${weatherData.weather[0].icon}.png`}
             alt={weatherData.weather[0].main}
           />
         </div>
-        {/* Display temperature, feels like temperature, humidity, and wind speed */}
+        {/*Display temperature, feels like temperature, humidity, and wind speed */}
         <div>
-          <p>Temperature: {weatherData.main.temp} °C</p>
+          <p>Temperature:{Math.round(weatherData.main.temp - 273.15)} °C </p>
           <p>Feels like: {weatherData.main.feels_like} °C</p>
           <p>Humidity: {weatherData.main.humidity}%</p>
           <p>Wind speed: {weatherData.wind.speed} m/s</p>
